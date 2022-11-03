b3a1121cfbce46204333dbc8ad4174457122e259
no_wan <56969728+nowan02@users.noreply.github.com>
2022-11-03 15:23:32 +0100

    Final

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace wheel_of_fortune
@@ -14,15 +14,15 @@ namespace wheel_of_fortune
         readonly string _vowels;
         readonly string[] _proverbs;
         readonly int[] _prizes;
-        string _chosenProverb;
-        char[] _chosenProverbArray;
+        string _chosenProverb; // The chosen proverb in string
+        char[] _chosenProverbArray; // An array of underlines which changes according to the guessed characters
         int[] _playerScores;
 
         public Game(string ProverbsPath)
         {
             _vowels = "aeiou";
             _proverbs = File.ReadAllLines(ProverbsPath);
-            _prizes = new int[] {500, 1000, 5000, 10000, 20000};  // The prizes are the current HUF banknotes in use :)
+            _prizes = new int[] { 500, 1000, 5000, 10000, 20000 };  // The prizes are the current HUF banknotes in use :)
         }
 
         /// <summary>
@@ -34,71 +34,71 @@ namespace wheel_of_fortune
         /// </summary>
         int _outGuess(char Guess, int Player)
         {
-        int _final = 0;
-        int _multiplier = 0;
-        int _prize = 0;
-    
-        _prize = _prizes[new Random().Next(0, _prizes.Length)];
-        Console.WriteLine("Player {0} spinned value {1} HUF", Player, _prize);
-        _multiplier = 0;
-        for(int i = 0; i < _chosenProverbArray.Length; i++)
-        {
-            if(_chosenProverb[i] == Guess)
+            int _final = 0;
+            int _multiplier = 0;
+            int _prize = 0;
+
+            _prize = _prizes[new Random().Next(0, _prizes.Length)];
+            Console.WriteLine("Player {0} spinned value {1} HUF", Player + 1, _prize);
+            _multiplier = 0;
+            for (int i = 0; i < _chosenProverbArray.Length; i++)
             {
-                _chosenProverbArray[i] = Guess;
-                _multiplier += 1;
-                Console.Write($"{Guess} ");
+                if (_chosenProverb[i] == Guess)
+                {
+                    _chosenProverbArray[i] = Guess;
+                    _multiplier += 1;
+                    Console.Write($"{Guess} ");
+                }
+                else
+                {
+                    Console.Write($"{_chosenProverbArray[i]} ");
+                }
             }
-            else
-            {
-                Console.Write($"{_chosenProverbArray[i] }");
-            }
-        }
-        Console.WriteLine("\nPlayer {0} earns {1}*{2} = {3} HUF", Player, _multiplier, _prize, _multiplier*_prize);
-        _final += _prize*_multiplier;
-        return _final;
+            Console.WriteLine("\nPlayer {0} earns {1}*{2} = {3} HUF", Player + 1, _multiplier, _prize, _multiplier * _prize);
+            _final += _prize * _multiplier;
+            return _final;
         }
 
         int _outGuess(string Guess, int Player)
         {
             int _multiplier = 0;
             int _prize = _prizes[new Random().Next(0, _prizes.Length)];
-            for(int i = 0; i < _chosenProverb.Length; i++)
+            for (int i = 0; i < _chosenProverb.Length; i++)
             {
-                if(_chosenProverb[i] == Guess[i]) _multiplier++;
+                if (_chosenProverb[i] == Guess[i]) _multiplier++;
             }
 
-            if(_multiplier == _chosenProverb.Length)
+            if (_multiplier == _chosenProverb.Length)
             {
-                for(int i = 0; i < _chosenProverbArray.Length; i++)
+                for (int i = 0; i < _chosenProverbArray.Length; i++)
                 {
                     _chosenProverbArray[i] = Guess[i];
                 }
-                Console.WriteLine("Player {0} guessed the sentence:\n{1}", Player, _chosenProverb);
-                Console.WriteLine("Player {0} wins {1}*{2} HUF", Player, _multiplier, _prize);
-                return _prize*_multiplier;
+                Console.WriteLine("Player {0} guessed the sentence:\n{1}", Player + 1, _chosenProverb);
+                Console.WriteLine("Player {0} wins {1}*{2} HUF", Player + 1, _multiplier, _prize);
+                return _prize * _multiplier;
             }
-            Console.WriteLine("Player {0}'s guess wasn't right.");
+            Console.WriteLine("Player {0}'s guess wasn't right.", Player + 1);
             return 0;
         }
 
         /// <summary>
         /// Initialises the game instance by choosing a random proverb and converting it to a character array.
         /// This array is only made up of underlines in the beginning, as no letters have been guessed so far.
-        /// The user is given the option to choose the number of players, if left blank it defaults to 1 player.
+        /// The user is given the option to choose the number of players.
         /// </summary>
         void _init()
         {
             Console.WriteLine("### Welcome to Wheel of Fortune! ###");
             Console.WriteLine("Firstly, please type in the number of players!\nIf you want to play by yourself, type 1!");
             int playerNum = 0;
-            while(playerNum == 0)
+            while (playerNum == 0)
             {
                 try
                 {
                     playerNum = int.Parse(Console.ReadLine());
                 }
-                catch(Exception)
+                catch (Exception)
                 {
                     Console.WriteLine("The entered character was not a number, try again!");
                 }
@@ -106,14 +106,17 @@ namespace wheel_of_fortune
             _playerScores = new int[playerNum];
             _chosenProverb = _proverbs[new Random().Next(0, _proverbs.Length)].ToLower();
             _chosenProverbArray = new char[_chosenProverb.Length];
-            
-            for(int i = 0; i < _chosenProverbArray.Length; i++)
+
+            for (int i = 0; i < _chosenProverbArray.Length; i++)
             {
-                if(_chosenProverb[i] != ' ') 
+                if (_chosenProverb[i] != ' ')
                 {
-                _chosenProverbArray[i] = '_';
-                } else _chosenProverbArray[i] = ' ';
+                    _chosenProverbArray[i] = '_';
+                }
+                else _chosenProverbArray[i] = ' ';
+                Console.Write($"{_chosenProverbArray[i]} ");
             }
+            Console.Write("\n");
         }
 
         /// <summary>
@@ -122,7 +125,7 @@ namespace wheel_of_fortune
         bool _compare()
         {
             string _sentence = "";
-            for(int i = 0; i < _chosenProverbArray.Length; i++)
+            for (int i = 0; i < _chosenProverbArray.Length; i++)
             {
                 _sentence += _chosenProverbArray[i];
             }
@@ -130,15 +133,24 @@ namespace wheel_of_fortune
             return _sentence == _chosenProverb;
         }
 
-        bool _checkForVowel(char Guess)
+        bool _checkForGuessed(char Guess)
         {
             int i = 0;
-            while(i < _vowels.Length && Guess != _vowels[i])
+            while (i < _chosenProverbArray.Length && _chosenProverbArray[i] != Guess)
             {
                 i++;
             }
-            if(i < _vowels.Length) return true;
-            return false;
+            return i < _chosenProverbArray.Length;
+        }
+
+        bool _checkForVowel(char Guess)
+        {
+            int i = 0;
+            while (i < _vowels.Length && Guess != _vowels[i])
+            {
+                i++;
+            }
+            return i < _vowels.Length;
         }
 
         /// <summary>
@@ -146,69 +158,82 @@ namespace wheel_of_fortune
         /// </summary>
         public void GameLoop()
         {
+            // Initial setup
             _init();
             int _round = 1;
             int _player = 0;
             int _win = 0;
             char _guess;
+            //Round loop
             do
             {
                 Console.WriteLine("Round {0}:", _round);
                 _guess = ' ';
-                do
+                //Player loop
+                while (_player < _playerScores.Length && _compare() == false)
                 {
-                    try
+                    //Individual player loop, when guesses are correct, it repeats
+                    do
                     {
-                        Console.WriteLine("Player {0}, please write the letter you want to guess, or type '0' if you'd like to guess the entire sentence!", _player+1);
-                        _guess = char.Parse(Console.ReadLine());
-                        if(_guess == '0')
+                        Console.WriteLine("Player {0}, please write the letter you want to guess, or type '0' if you'd like to guess the entire sentence!", _player + 1);
+                        try
                         {
-                            Console.WriteLine("Player {0} would like to guess the entire sentence.", _player+1);
-                            _playerScores[_player] += _outGuess(Console.ReadLine().ToLower(), _player);
-                        }
-                        else
-                        { 
-                            if(_checkForVowel(_guess))
+                            _guess = char.Parse(Console.ReadLine());
+                            if (_guess == '0')
                             {
-                                Console.WriteLine("Vowels can't be guessed, try again!");
+                                Console.WriteLine("Player {0} would like to guess the entire sentence.", _player + 1);
+                                _playerScores[_player] += _outGuess(Console.ReadLine().ToLower(), _player);
+                            }
+                            else
+                            {
+                                if (_checkForVowel(_guess))
+                                {
+                                    Console.WriteLine("Vowels can't be guessed, try again!");
+                                    _guess = ' ';
+                                    _win = -1;
+                                    continue;
+                                }
+                                else if (_checkForGuessed(_guess))
+                                {
+                                    Console.WriteLine("You can't guess a letter that was already guessed, try again!");
+                                    _guess = ' ';
+                                    _win = -1;
+                                    continue;
+                                }
+                                Console.WriteLine("Player {0} guessed \"{1}\"", _player + 1, _guess);
+                                _win = _outGuess(_guess, _player);
+                                _playerScores[_player] += _win;
                                 _guess = ' ';
-                                continue;
+                                if (_win != 0) Console.WriteLine("Player {0} takes another turn.", _player + 1);
                             }
                         }
-                        Console.WriteLine("Player {0} guessed \"{1}\"", _player+1, _guess);
-                        _win = _outGuess(_guess, _player);
-                        _playerScores[_player] += _win;
-                        _guess = ' ';
-                        if(_win != 0) Console.WriteLine("Player {0} takes another turn.", _player+1);
+                        catch (InvalidCastException)
+                        {
+                            Console.WriteLine("The input wasn't a valid character, try again!");
+                        }
                     }
-                    catch(Exception)
-                    {
-                        Console.WriteLine("The input wasn't a valid character, try again!");
-                    }
+                    while (_guess == ' ' && _win != 0);
+                    _win = 0;
+                    _player++;
                 }
-                while (_guess == ' ' && _win != 0);
+                _player = 0;
                 _round++;
-                _win = 0;
             }
-            while(_compare() == false);
+            while (_compare() == false);
             // End of game
             Console.WriteLine("######## GAME OVER ########");
-            Console.WriteLine("Player {0} won the entire game and took home {1} HUF", _player, _playerScores[_player]);
-            Console.WriteLine("Would you like to start over? Type 'yes' or 'no'");
+            Console.WriteLine("Player {0} won the entire game and took home {1} HUF", _player + 1, _playerScores[_player]);
             // Game restart option
             string _reset = "";
-            while(_reset != "yes" || _reset != "no")
+            while (_reset != "yes" && _reset != "no")
             {
+                Console.WriteLine("Would you like to start over? Type 'yes' or 'no'");
                 _reset = Console.ReadLine();
             }
 
-            if(_reset == "yes")
+            if (_reset == "yes")
             {
                 GameLoop();
-            }
-            else
-            {
-                Console.WriteLine("Goodbye!");
             }
         }
     }
@@ -219,6 +244,7 @@ namespace wheel_of_fortune
         {
             Game game1 = new Game("proverbs.txt");
             game1.GameLoop();
+            Console.WriteLine("Goodbye!");
         }
     }
 }
